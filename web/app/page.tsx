32fff7de896f7d7dbdf8d0da364d34133e84becb
guilherme.dantas <guilherme.dantas@pixeon.com>
2026-01-23 09:22:47 -0300

    Fix: dynamic page.tsx

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -7,6 +7,7 @@ import { getSystemStats } from './actions';
 import Footer from '@/src/components/Footer';
 
 export const revalidate = 30;
+export const dynamic = 'force-dynamic';
 
 export default async function Home() {
   const stats = await getSystemStats();
