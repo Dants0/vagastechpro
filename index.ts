b0b9e2bf6ab74a16a68bb74d693af93a06d0a319
Dants0 <guilhermedantasgoes@gmail.com>
2026-01-28 00:06:31 -0300

    feat: bypass no cron de scrappear para dar vasão às vagas existentes

--- index.ts
+++ index.ts
@@ -27,17 +27,17 @@ const worker = new QueueWorker(bot, TARGET_CHANNEL_ID);
 
 // --- AGENDAMENTOS ---
 
-// 1. Scraper Padrão (Leve): A cada 4 horas (ex: 8h, 12h, 16h...)
-cron.schedule('0 */4 * * *', async () => {
-  console.log('⏰ Cron: Rodando Scraper Padrão...');
-  await scraper.run();
-});
+// // 1. Scraper Padrão (Leve): A cada 4 horas (ex: 8h, 12h, 16h...)
+// cron.schedule('0 */4 * * *', async () => {
+//   console.log('⏰ Cron: Rodando Scraper Padrão...');
+//   await scraper.run();
+// });
 
-// 2. Scraper Pesado (Hard): Apenas 3x ao dia (Manhã, Tarde, Noite)
-cron.schedule('0 9,15,21 * * *', async () => {
-  console.log('⏰ Cron: Rodando Hard Scraper...');
-  await hardScraper.run();
-});
+// // 2. Scraper Pesado (Hard): Apenas 3x ao dia (Manhã, Tarde, Noite)
+// cron.schedule('0 9,15,21 * * *', async () => {
+//   console.log('⏰ Cron: Rodando Hard Scraper...');
+//   await hardScraper.run();
+// });
 
 // 3. Worker (Postador): A cada 1 hora
 // Ele verifica se tem vaga na fila e posta 1. Isso evita "flood" no canal.
